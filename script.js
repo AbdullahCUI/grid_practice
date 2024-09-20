4f2f6d466c5f5298fabb9dfa104389f1324bd391
Abdullah-CUI <abdullahdev88@gmail.com>
2024-09-20 22:29:30 +0500

    practice on grid

---
+++ script.js
@@ -0,0 +1,10 @@
+function myfunction() {
+	let showmenu= document.getElementById('mylinks');
+
+	if(showmenu.style.display==="block") {
+		showmenu.style.display="none";
+	}
+	else {
+		showmenu.style.display="block";
+	}
+}
